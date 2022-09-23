180ea6162442e1f66a3fd0364293f9ce50d15161
Revanth Cherukuri <rev.cherukuri@gmail.com>
2022-09-22 18:06:14 -0700

    Delete DoubleTransposition.java

--- src/DoubleTransposition.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class DoubleTransposition {
-    public static void main (String [] args) {
-        Scanner read = new Scanner (System.in);
-        System.out.println("Ciphertext:\n");
-        String ciphertext = read.next();
-    }
-}
